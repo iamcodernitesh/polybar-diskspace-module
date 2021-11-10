ff15189b58fccc8a5fa413f30458f90fd849d627
Nitesh Kumar <i.am.coder.nitesh@gmail.com>
2021-11-11 00:12:33 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ mkdir $HOME/.config/polybar/scripts/
 mkdir $HOME/.config/polybar/scripts/diskspace
 cat module >> $HOME/.config/polybar/config 
 cp diskspace.sh $HOME/.config/polybar/scripts/diskspace/
+chmod 755 $HOME/.config/polybar/scripts/diskspace/diskspace.sh
